346c9604c07c7f29425b0a644a443f01e33934f8
Gabriel Burgos <gabrielburgosmm@gmail.com>
2022-06-20 13:47:18 -0700

    commit tree 100692caf995955dec0e7a75fc839dee4c130633

---
+++ index-test.js
@@ -0,0 +1,11 @@
+describe("shout(string)", function () {
+    // there's stuff in here, too
+  });
+
+  describe("shout(string)", function () {
+    it("receives one argument and returns it in all caps", function () {
+      // we'll get to this in a sec
+    });
+  });
+
+  expect(shout("hello")).toEqual("HELLO");
--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Follow along with the examples here
+function sayHello() {
+    console.log("Hello!");
+  }
+
+  sayHello();
+
+  function doSomething(thing) {
+    console.log(thing);
+  }
+  
+  doSomething("I'm your father");
+
+  function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }
+
+  say("Gabriel" , "Hola")
+
+  function add(x, y) {
+    return x + y;
+  }
+
+  console.log(add(100, 50))
+
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log("I was called!");
+  }
+
+  say("Well hello" , "Laura")
